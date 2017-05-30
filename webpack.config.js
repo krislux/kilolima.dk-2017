e079553d13f9f555fd314886fa72adb1a2a32368
Kris <kl@republica.dk>
2017-05-30 11:10:09 +0200

    Second version

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// Libs
+let ExtractText = require('extract-text-webpack-plugin');
+let webpack = require('webpack');
+
+// Define settings
+module.exports = {
+    // The main .js file path
+    entry: {
+        'app': './client/js/app.js'
+    },
+
+    // Define loaders
+    module: {
+        loaders: [
+            // Sass
+            {
+                test: /\.scss$/,
+                loader: ExtractText.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader?minimize!sass-loader'
+                })
+            },
+            
+            // ES6
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+
+    // Output .js file
+    output: {
+        filename: 'public/js/[name].js',
+        sourceMapFilename: '[file].map'
+    },
+
+    // plugins
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: true,
+            extractComments: false,
+            sourceMap: true
+        }),
+        new ExtractText('public/css/[name].css')
+    ],
+    
+    devtool: '#cheap-module-source-map'
+};
