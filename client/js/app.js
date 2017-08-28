be0a18c679c6bd38bd24bf8c432797728bbb64f1
Kris <kl@republica.dk>
2017-08-28 15:07:51 +0200

    Added contact links, only optimized for desktop so far

--- client/js/app.js
+++ client/js/app.js
@@ -1,2 +1,19 @@
 "use strict";
 require('../sass/main.scss');
+
+window.addEventListener('DOMContentLoaded', function() {
+    var mlStr = 'bWFpbHRvOm1haWxAa2lsb2xpbWEuZGs';
+    var menuItems = document.querySelectorAll('menu > li');
+    menuItems.forEach(function (el) {
+        if (el.dataset.url) {
+            el.addEventListener('click', function() {
+                window.open(el.dataset.url);
+            })
+        }
+        if (el.dataset.action == 'mail') {
+            el.addEventListener('click', function() {
+                location.href = atob(mlStr);
+            })
+        }
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,11 @@ module.exports = {
                 })
             },
             
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=../fonts/[name].[ext]'
+            },
+            
             // ES6
             {
                 test: /\.js$/,
