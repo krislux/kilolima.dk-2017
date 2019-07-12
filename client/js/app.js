88bfd80194f3b118f8bb45356afaab05806af0a9
Kris Lux <kris.lux@eksponent.com>
2019-07-12 10:19:09 +0200

    Minor accessibility update to mail link, changed ul to menu

--- client/js/app.js
+++ client/js/app.js
@@ -1,13 +1,14 @@
 "use strict";
 require('../sass/main.scss');
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', () => {
     let mlStr = 'bWFpbHRvOm1haWxAa2lsb2xpbWEuZGs';
-    let menuItems = document.querySelectorAll('.menu > li');
+    let menuItems = document.querySelectorAll('.menu li');
     menuItems.forEach(el => {
         if (el.dataset.action == 'mail') {
-            el.addEventListener('click', () => {
-                location.href = atob(mlStr);
+            el.addEventListener('click', event => {
+                event.preventDefault();
+                window.location.href = atob(mlStr);
             });
         }
     });
